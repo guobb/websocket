ae7cccafde01423a7fcb28b169ffe09a35b629c5
guobb <18576469764@163.com>
2017-02-22 16:51:20 +0800

    going chat

--- chat/server.js
+++ chat/server.js
@@ -18,18 +18,50 @@ const server = require('http').createServer(app);
 // 再创建scoket.io服务器
 const io = require('socket.io')(server);
 
-let clients = [];
+let clients = {};
+
 // 监听客户端的连接事件
 // socket代表与某个客户端的连接对象
 io.on('connection', (socket) => {
+
+    let username;
+
+    socket.send({user: '系统消息', content: '请输入用户名'});
+
     // 把socket缓存起来
-    clients.push(socket);
+
     // 监听客户端的消息
     socket.on('message', (msg) => {
-        //  把客户端发过来的消息广播给所有的客户端
-        clients.forEach((client) => {
-            client.send(msg)
-        })
+
+        let result = msg.match(/^@(.+)\s(.+)$/);
+
+        if (result) {
+            let toUser = result[1];
+            let content = result[2];
+
+            if (clients[toUser]) { // 通过用户名把对应的socket取出来
+                clients[toUser].send({user: username, content:'[私聊]'+ content})
+            } else {
+                socket.send({user: '系统消息', content: '你私聊的人没在线' });
+            }
+        } else {
+
+            if (username) {
+                //  把客户端发过来的消息广播给所有的客户端
+                for(s in clients){
+                    clients[s].send({user: username, content: msg});
+               /* clients.forEach((client) => {
+                    client.send({user: username, content: msg});*/
+                }
+            } else {
+                username = msg;
+                // 属性名是用户名， 值是对应的socket对象
+                clients[username] = socket;
+                socket.send({user: '系统消息', content: '你的用户名修改为:' + username});
+            }
+
+        }
+
     })
 
 });
